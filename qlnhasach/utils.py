acaeb6eb85cfa2c39e5b1a6a0c99773ebb02a5c7
vananh1101 <dnvananh1101@yahoo.com>
2020-12-08 17:52:10 +0700

    phieu nhap sach

--- qlnhasach/utils.py
+++ qlnhasach/utils.py
@@ -8,14 +8,3 @@ def read_data():
     return dssach.all()
 
 
-def nhap_sach(soLuongNhap, idSachNhap):
-    minNhap = QuyDinh.query.value(QuyDinh.so_luong_nhap_toi_thieu)
-    maxSachTon = QuyDinh.query.value(QuyDinh.so_luong_ton_toi_thieu)
-    soLuongTon = db.session.query(Sach.so_luong).filter(Sach.id == idSachNhap).value(Sach.so_luong)
-    update = Sach.query.filter(Sach.id == idSachNhap).first()
-    try:
-        if soLuongNhap >= minNhap and soLuongTon < maxSachTon:
-            update.so_luong += soLuongNhap
-            db.session.commit()
-    except Exception as ex:
-        print(ex)
